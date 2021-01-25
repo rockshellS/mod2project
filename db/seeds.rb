cb34cadf044eb9e38018e769c27764a7d6e7baea
“Abbie <abbie.coghlan@gmail.com>
2021-01-25 16:15:21 -0600

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+abbie = User.create(name: "Abbie", user_name: "thatsabbie", bio: "This is Abbie's bio!")
+rachelle = User.create(name: "Rachelle", user_name: "hirachelle", bio: "This is Rachelle's bio!")
+aaron = User.create(name: "Aaron", user_name: "heyaaron", bio: "This is Aaron's bio!")
+
+listing_one = Listing.create(title: "crockpot", description: "this is a crockpot that i do not want please come pick it up ASAP", category: "kitchen", status: "claimed", owner_id: 1, recipient_id: 2)
+listing_two = Listing.create(title: "crockpot", description: "this is a crockpot that i do not want please come pick it up ASAP", category: "kitchen", status: "not yet claimed", owner_id: 1, recipient_id: nil)
+
+new_like = Like.create(liker_id: 2, listing_id:1)
+new_like_two = Like.create(liker_id: 3, listing_id:2)
