4a202082e057c7d10209a1d7383819c9b74de8d1
edougher <ejdougher@gmail.com>
2021-01-25 17:58:43 -0600

    temp

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ApplicationRecord
     belongs_to :owner, :class_name => :User
 
-    belongs_to :recipient, :class_name => :User, optional: true
+    belongs_to :recipient, :class_name => :User, optional:true
 
     has_many :likes
 
