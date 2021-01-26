e609fd70582788340b3edb7b704ae31c199bb3b6
edougher <ejdougher@gmail.com>
2021-01-26 11:13:18 -0600

    stuff 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  #resources :likes
-  #resources :listings
-  #resources :users
-  #get '/listing', to: index
-  resources :listings, only: :index
+  resources :likes
+  resources :listings
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
