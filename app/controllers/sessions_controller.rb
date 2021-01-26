4daec2b030c24597aa52b3e15979ff03c3c48300
“Abbie <abbie.coghlan@gmail.com>
2021-01-26 14:34:48 -0600

    added login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+    def new
+    end 
+
+    def create 
+        user = User.find_by(username: params[:username])
+
+        if user.try(:authenticate, params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else 
+            flash.alert = ["Incorrect username or password."]
+            redirect_to new_session_path
+        end 
+
+    end 
+
+    # def create 
+    #     user = User.find_by(username: params[:username])
+
+    #     if user.authenticate(params[:password])
+    #         redirect_to user_path(user)
+    #     else 
+    #         redirect_to new_session_path
+    #     end 
+
+    # end 
+
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
 
     has_many :likes, :foreign_key => 'liker_id'
 
+    has_secure_password
    
 end
