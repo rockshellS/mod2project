05f03ce8d7d32523269ab464809ef8d0425c70e3
Abbie <abbie.coghlan@gmail.com>
2021-01-27 16:35:04 -0600

    fixed recipient drop down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    before_action :require_login
+
     def current_user
-        session[:username]
+        User.find(session[:user_id]) if session[:user_id]
     end 
 
 
-    # def require_login
-    #     redirect_to controller: 'sessions', action: 'new' unless current_user
-    # end 
+    def require_login
+        redirect_to controller: 'sessions', action: 'new' unless current_user
+    end 
 
 end
