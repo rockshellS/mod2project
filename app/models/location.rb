9fedcdd8f4ddf48472272baab2d74e2b3b97c6fb
“Abbie <abbie.coghlan@gmail.com>
2021-01-27 19:55:22 -0600

    neighborhoods and locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ApplicationRecord
     has_many :users
-    has_many :listings, through: :users
+    has_many :listed_items, :class_name => :Listing, through: :users
     validates_uniqueness_of :neighborhood
 end
