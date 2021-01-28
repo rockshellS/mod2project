34e43ca4a35ecd61e25894d9c7180052cbd8672d
Abbie <abbie.coghlan@gmail.com>
2021-01-27 19:12:37 -0600

    added location and updated

--- db/migrate/20210125212820_create_users.rb
+++ db/migrate/20210125212820_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :username
       t.string :password_digest
       t.text :bio
+      t.string :location_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_171343) do
+ActiveRecord::Schema.define(version: 2021_01_28_004203) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,11 +36,18 @@ ActiveRecord::Schema.define(version: 2021_01_26_171343) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.string "neighborhood"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
     t.string "password_digest"
     t.text "bio"
+    t.string "location_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
